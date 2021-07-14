f860a66719977194df79dff5de16c2cc3ee9adc2
gabinwilliams <scoutsnknives@gmail.com>
2021-07-14 16:11:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          sign up
+        </Route>
+        <Route path="/signin">
+	  	  sign in
+        </Route>
+        <Route path="/signout">
+          sign out 
+        </Route>
+        <Route path="/">
+          Home
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
